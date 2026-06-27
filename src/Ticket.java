9ac81b3555fdb56cbfced7647d89460942d34b42
sekibaalamark <markmajje256@gmail.com>
2026-06-27 18:22:57 +0300

    Create Constructor for Ticket Class

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,2 +1,26 @@
-public class Ticket {
+
+class Ticket {
+    private String username;
+    private String contactInformation;
+    private String category;
+    private String dateCreated;
+    private String description;
+    private String comment;
+    private String status;
+    private int priority;
+
+    Ticket(String username,String contactInformation, String category, String dateCreated,String description,
+           String comment, int priority){
+        this.username = username;
+        this.contactInformation = contactInformation;
+        this.category = category;
+        this.dateCreated = dateCreated;
+        this.description = description;
+        this.comment = comment;
+        this.priority = priority;
+        this.status = "Pending";
+    }
+
+
+
 }
